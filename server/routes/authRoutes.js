d7f95d4af484f7a0eb13a733ad1014d8f732a03f
AjayMSPradhan11 <ajaymsp2021@protonmail.com>
2025-01-19 13:34:11 +0545

    Third Commit

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -59,38 +59,6 @@ function computePearsonCorrelation(category1, category2) {
   return den === 0 ? 0 : num / den;
 }
 
-// Get recommendations based on user preferences and categories
-async function getRecommendations(userPreferences) {
-  // Fetch all users' preferences from the database
-  const allUsers = await db.query("SELECT id, preferred_cuisines FROM users");
-
-  const similarityScores = allUsers.map(user => {
-    const userPrefs = JSON.parse(user.preferences); // Assume preferences are stored as JSON
-    return {
-      userId: user.id,
-      similarity: computePearsonCorrelation(userPreferences, userPrefs),
-    };
-  });
-
-  // Sort users by similarity in descending order
-  similarityScores.sort((a, b) => b.similarity - a.similarity);
-
-  // Get the preferences of the most similar user
-  const topUser = allUsers.find(u => u.id === similarityScores[0].userId);
-  const topUserPreferences = JSON.parse(topUser.preferences);
-
-  // Recommend recipes that the top user likes but the current user has not rated
-  const recommendations = Object.keys(topUserPreferences)
-    .filter(recipe => !userPreferences[recipe] && topUserPreferences[recipe] > 0)
-    .map(recipeId => ({
-      id: recipeId,
-      name: getRecipeNameById(recipeId), // Fetch recipe details
-      image: getRecipeImageById(recipeId),
-    }));
-
-  return recommendations;
-}
-
 // Display login page
 router.get("/login", (req, res) => {
   res.render("login", { error: null });
@@ -129,5 +97,36 @@ router.post("/login", async (req, res) => {
     res.render("login", { error: "Login failed. Please try again." });
   }
 });
+
+async function getRecommendations(userPreferences) {
+  // Fetch all users' preferences from the database
+  const allUsers = await db.query("SELECT id, preferred_cuisines FROM users");
+
+  const similarityScores = allUsers.map(user => {
+    const userPrefs = JSON.parse(user.preferences); // Assume preferences are stored as JSON
+    return {
+      userId: user.id,
+      similarity: computePearsonCorrelation(userPreferences, userPrefs),
+    };
+  });
+
+  // Sort users by similarity in descending order
+  similarityScores.sort((a, b) => b.similarity - a.similarity);
+
+  // Get the preferences of the most similar user
+  const topUser = allUsers.find(u => u.id === similarityScores[0].userId);
+  const topUserPreferences = JSON.parse(topUser.preferences);
+
+  // Recommend recipes that the top user likes but the current user has not rated
+  const recommendations = Object.keys(topUserPreferences)
+    .filter(recipe => !userPreferences[recipe] && topUserPreferences[recipe] > 0)
+    .map(recipeId => ({
+      id: recipeId,
+      name: getRecipeNameById(recipeId), // Fetch recipe details
+      image: getRecipeImageById(recipeId),
+    }));
+
+  return recommendations;
+}
 
 module.exports = router;
---
+++ server/routes/getRecommendation.js
@@ -0,0 +1,40 @@
+// getrecommendation.js
+
+const db = require("../models/database"); // Import the database pool
+const { computePearsonCorrelation } = require('../routes/authRoutes');
+
+// Get recommendations based on user preferences and categories
+async function getRecommendations(userPreferences) {
+  // Fetch all users' preferences from the database
+  const allUsers = await db.query("SELECT id, preferred_cuisines FROM users");
+
+  const similarityScores = allUsers.map(user => {
+    const userPrefs = JSON.parse(user.preferences); // Assume preferences are stored as JSON
+    return {
+      userId: user.id,
+      similarity: computePearsonCorrelation(userPreferences, userPrefs),
+    };
+  });
+
+  // Sort users by similarity in descending order
+  similarityScores.sort((a, b) => b.similarity - a.similarity);
+
+  // Get the preferences of the most similar user
+  const topUser = allUsers.find(u => u.id === similarityScores[0].userId);
+  const topUserPreferences = JSON.parse(topUser.preferences);
+
+  // Recommend recipes that the top user likes but the current user has not rated
+  const recommendations = Object.keys(topUserPreferences)
+    .filter(recipe => !userPreferences[recipe] && topUserPreferences[recipe] > 0)
+    .map(recipeId => ({
+      id: recipeId,
+      name: getRecipeNameById(recipeId), // Fetch recipe details
+      image: getRecipeImageById(recipeId),
+    }));
+
+  return recommendations;
+}
+
+module.exports = { 
+  getRecommendations 
+};
